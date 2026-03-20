6b74d58b192f276b872c461348e2fed330d1fe0a
Mason Trang <mason.trang@gmail.com>
2026-03-20 15:38:08 -0700

    1.2.1

--- update-version.js
+++ update-version.js
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Read current version from package.json
-const packagePath = path.join(__dirname, 'package.json');
-const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+const packagePath = path.join(__dirname, "package.json");
+const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
 const currentVersion = packageJson.version;
 
 // Update AppMasthead.vue with the new version
-const mastheadPath = path.join(__dirname, 'src/components/layout/AppMasthead.vue');
-const mastheadContent = fs.readFileSync(mastheadPath, 'utf8');
+const mastheadPath = path.join(__dirname, "src/components/layout/AppMasthead.vue");
+const mastheadContent = fs.readFileSync(mastheadPath, "utf8");
 
 // Replace the version line
 const updatedContent = mastheadContent.replace(
   /const version = "[\d.]+"; \/\/ This will be updated automatically by deployment scripts/,
-  `const version = "${currentVersion}"; // This will be updated automatically by deployment scripts`
+  `const version = "${currentVersion}"; // This will be updated automatically by deployment scripts`,
 );
 
 fs.writeFileSync(mastheadPath, updatedContent);
